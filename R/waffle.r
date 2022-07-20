c838571e588b7382245955f0874e4b8bbe3d3627
Jiří Moravec <jiri.c.moravec@gmail.com>
2022-07-20 16:47:49 +1200

    Added unstacked waffles

--- R/waffle.r
+++ R/waffle.r
@@ -2,28 +2,33 @@
 #'
 #' Waffle chart is a pie chart that visually represents abundances with the number of squares.
 #' The waffle chart consists of squares plotted on a rectangular lattice according to a design
-#' matrix that is constructed automatically from a vector of abundances or can be provided directly.
+#' matrix that is constructed from a vector of abundances or can be provided directly.
 #'
 #' @details
-#' The `waffle()` function accepts a vector of abundances and constructs the design matrix
-#' using the `design()` function, which is then parsed to the `waffle.mat()` function.
+#' The `waffle()` function accepts a vector of abundances and plots a waffle chart.
+#' This is done by first constructing a design matrix using the `design()` function,
+#' which is then parsed to the `waffle.mat()` function.
 #'
-#' The function `design()` construct the design matrix according to specification.
+#' The `design()` functions construct a design matrix according.
 #' The design matrix is filled by an integer vector derived from the abundance vector `x`.
-#' Each integer correspond to the order of abundances in `x`, with the quantity equal to its value
+#' Each integer correspond to the order of abundances in `x`, with the quantity equal to the value
 #' in `x`. This means that for the `x=c(3,5)`, the design matrix will be filled with three `1`
 #' and five `2`, all other cells of the design matrix are set as unknown values `NA`.
 #'
 #' By default, the design matrix is filled by row, starting from the bottom left corner,
-#' this can be changed by setting the variables `byrow`, `bottom` and `left`.
-#' By setting `byrow=FALSE`, the matrix is filled by columns first, if `bottom=FALSE`, the matrix
-#' is filled starting from upper left corner and so on.
+#' this can be changed by setting the variables `byrow` and `from`.
+#' By setting `byrow=FALSE`, the matrix is filled by columns first.
 #'
 #' If `ncol` or `nrow` is not specified, a squared matrix that will fit the sum of abundances
 #' will be constructed.
 #'
-#' In addition, if the input vector `x` is a named vector, the names are preserved in the
-#' `levels` attribute. These levels are not currently used, but might be used in the future
+#' By default, the design matrix is stacked, i.e., all the elements are placed in a single
+#' matrix object, similarly how barplot produces stacked lines. By specifying `stack=FALSE`,
+#' the `design()` unstacks the elements, each are then placed in their own matricies, which
+#' are then connected by `NA` elements of size `gap`. More complex 
+#'
+#' If the input vector `x` is a named vector, the names are preserved in the `levels`
+#' attribute. These levels are not currently used, but might be used in the future
 #' for automatic legend creation and subsetting.
 #'
 #' The function `waffle.mat()` accepts a custom-made design matrix and thus allows a better
@@ -49,6 +54,10 @@
 #' @param byrow **optional** fill matrix by rows or by columns
 #' @param from **optional** starting position from which the matrix is filled, one of
 #'   "bottomleft", "bottomright", "topleft" or "topright".
+#' @param stacked **optional** if FALSE, produce an unstacked waffle chart, see details
+#' @param gap **optional** gap between unstacked subwaffles, works only for unstacked waffle charts
+#' @param horiz **optional** if FALSE, vertical instead of horizontal waffle chart is produced,
+#'   works only for unstacked waffle charts
 #' @param add **optional** whether to add to a current plot
 #' @param f **optional** a shape function (see details)
 #' @param ... **optional** other parameters passed to the `f` function
@@ -73,10 +82,17 @@
 #' design_mat[7,1:5] = 3
 #' waffle.mat(design_mat, col=cols)
 #' @export
-waffle = function(x, f=NULL, ..., nrow=NULL, ncol=NULL, byrow=TRUE, from="bottomleft", add=FALSE){
+waffle = function(
+    x, f=NULL, ...,
+    nrow=NULL, ncol=NULL, byrow=TRUE, from="bottomleft",
+    stacked=TRUE, gap=0, horiz=TRUE, add=FALSE
+    ){
 
     # construct design matrix
-    mat = design(x, nrow=nrow, ncol=ncol, byrow=byrow, from=from)
+    mat = design(
+        x, nrow=nrow, ncol=ncol, byrow=byrow, from=from,
+        stacked=stacked, gap=gap, horiz=horiz
+        )
 
     # plot the waffle using the design matrix
     waffle.mat(mat, f=f, ..., add=add)
@@ -123,9 +139,50 @@ waffle.mat = function(x, f=square, ..., add=FALSE){
     }
 
 
+
+#' Recycle dots args
+#'
+#' @param x an integer vector, typically with values ranging from 1 to n, with each integer
+#'   with each integer specifying an index of element for each item in `...`
+#' @param ... additional arguments, if length of item of `...` is larger than one, its elements are
+#'   is recycled according to indiced specified by the vector `x`
+#' @return recycled arguments of `...`
+#' @keywords internal
+recycle_dots = function(x, ...){
+    recycle = function(x, vector, length){
+        if(length(x) > 1){
+            x = rep(x, length.out=length)
+            x = x[vector]
+            }
+
+        x
+        }
+
+    n = max(x) # TODO integrate with levels later
+
+    dots = list(...)
+
+    dots[] = lapply(dots, recycle, vector=x, length=n)
+
+    dots
+    }
+
 #' @rdname waffle
 #' @export
-design = function(x, nrow=NULL, ncol=NULL, byrow=TRUE, from="bottomleft"){
+design = function(
+    x, nrow=NULL, ncol=NULL, byrow=TRUE, from="bottomleft",
+    stacked=TRUE, horiz=FALSE, gap=0
+    ){
+    if(stacked){
+        design_stacked(x, nrow=nrow, ncol=ncol, byrow=byrow, from=from)
+        } else {
+        design_unstacked(x, nrow=nrow, ncol=ncol, byrow=byrow, from=from, horiz=horiz, gap=gap)
+        }
+    }
+
+
+# internal
+design_stacked = function(x, nrow=NULL, ncol=NULL, byrow=TRUE, from="bottomleft"){
     from = match.arg(from, c("bottomleft", "bottomright", "topleft", "topright"))
 
     total = sum(x)
@@ -167,29 +224,43 @@ design = function(x, nrow=NULL, ncol=NULL, byrow=TRUE, from="bottomleft"){
     }
 
 
-#' Recycle dots args
-#'
-#' @param x an integer vector, typically with values ranging from 1 to n, with each integer
-#'   with each integer specifying an index of element for each item in `...`
-#' @param ... additional arguments, if length of item of `...` is larger than one, its elements are
-#'   is recycled according to indiced specified by the vector `x`
-#' @return recycled arguments of `...`
-#' @keywords internal
-recycle_dots = function(x, ...){
-    recycle = function(x, vector, length){
-        if(length(x) > 1){
-            x = rep(x, length.out=length)
-            x = x[vector]
-            }
+# internal
+design_unstacked = function(
+    x, nrow=NULL, ncol=NULL, byrow=TRUE, from="bottomleft", gap=0, horiz=FALSE
+    ){
+    if(is.null(nrow) && !horiz)
+        nrow = max(ceiling(sqrt(x)))
 
-        x
+    if(is.null(ncol) && horiz)
+        ncol = max(ceiling(sqrt(x)))
+
+    n = length(x)
+    template = vector("numeric", length=n)
+    mat = vector("list", length=n)
+    for(i in seq_len(n)){
+        y = template
+        y[i] = x[i]
+        
+        mat[[i]] = design(y, nrow=nrow, ncol=ncol, byrow=byrow, from=from)
         }
+    mat
+    
+    if(horiz){
+        empty = matrix(NA, ncol=ncol, nrow=gap)
+        mat[[n+1]] = empty
+        mat = mat[interleave(n+1, seq_len(n))[-1]]
+        mat = do.call(rbind, mat)
+        } else {
+        empty = matrix(NA, nrow=nrow, ncol=gap)
+        mat[[n+1]] = empty
+        mat = mat[interleave(n+1, seq_len(n))[-1]]
+        mat = do.call(cbind, mat)
+        }
+        
+    mat
+    }
 
-    n = max(x) # TODO integrate with levels later
-
-    dots = list(...)
-
-    dots[] = lapply(dots, recycle, vector=x, length=n)
-
-    dots
+# internal
+interleave = function(...){
+    as.vector(rbind(...))
     }
